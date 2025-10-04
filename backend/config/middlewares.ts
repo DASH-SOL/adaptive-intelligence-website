e2e6de5a1422ef0e3f8701a8335ce2e0f9be3965
shazilkhan <shazilh225@gmail.com>
2025-10-04 19:20:41 +0500

    middleware

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,9 +1,34 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            '*.onrender.com', // Allows images from your Render domains
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            '*.onrender.com', // Allows media from your Render domains
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
