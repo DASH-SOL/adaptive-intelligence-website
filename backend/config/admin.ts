bf15dec68f1dae74a25559ed211ec98dfa6502dc
shazilkhan <shazilh225@gmail.com>
2025-10-04 19:55:26 +0500

    mroe fixessss

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -11,9 +11,4 @@ module.exports = ({ env }) => ({
     },
   },
   url: env('ADMIN_URL', '/admin'),
-  // Add this to trust the proxy
-  forgotPassword: {
-    from: env('EMAIL_FROM'),
-    replyTo: env('EMAIL_REPLY_TO'),
-  },
 });
--- backend/config/server.ts
+++ backend/config/server.ts
@@ -4,10 +4,6 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // Add this configuration
   proxy: true,
   url: env('PUBLIC_URL', 'https://adaptive-intelligence-website-1.onrender.com'),
-  admin: {
-    url: '/admin',
-  },
 });
