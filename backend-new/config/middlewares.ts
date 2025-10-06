e156c4e4dc6ea81d0df5d04576216a60a0d0bff2
shazilkhan <82261242+shazilkhan@users.noreply.github.com>
2025-10-06 06:49:59 +0500

    middle ware fixes

--- backend-new/config/middlewares.ts
+++ backend-new/config/middlewares.ts
@@ -1,12 +1,17 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::session',
+    config: {
+      key: 'strapi.sid',
+      secure: false,  // Disable secure requirement
+      sameSite: 'lax',
+      rolling: false,
+      renew: false,
+      proxy: true,  // Tell koa-session to trust proxy
+    },
+  },
   'strapi::security',
   'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  // ... rest of middleware
 ];
