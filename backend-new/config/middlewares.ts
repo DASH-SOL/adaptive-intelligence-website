748ca342f4c13fa313b55aeb4dd579495abee055
shazilkhan <82261242+shazilkhan@users.noreply.github.com>
2025-10-06 06:51:53 +0500

    newmiddle

--- backend-new/config/middlewares.ts
+++ backend-new/config/middlewares.ts
@@ -13,5 +13,14 @@ export default [
   },
   'strapi::security',
   'strapi::cors',
-  // ... rest of middleware
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
